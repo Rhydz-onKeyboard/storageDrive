d161e8d12b7437f7ddf3c57855fc12e4b5912978
aldiaza <alr.diaza@gmail.com>
2023-05-10 16:11:28 -0400

    app update port

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const routes = require('./src/routes/index.routes.js');
 
 require('dotenv').config();
 
-const port = process.env.SERVER_PORT || 3000;
+const port = process.env.PORT || 3000;
 server.use('/', routes);
 server.listen(port, () => {
   console.log('Running on port', port);
