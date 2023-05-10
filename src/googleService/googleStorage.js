2804298d177a6377f5778cd3cef88739b3e1ad07
aldiaza <alr.diaza@gmail.com>
2023-05-10 18:19:38 -0400

    test

--- src/googleService/googleStorage.js
+++ src/googleService/googleStorage.js
@@ -24,7 +24,7 @@ module.exports = {
   uploadFile: async (fileName, content) => {
     const media = {
       mimeType: 'application/pdf',
-      body: content,
+      body: fs.createReadStream(content),
     }
     const response = await new Promise((resolve, reject) => {
       drive.files.create({
